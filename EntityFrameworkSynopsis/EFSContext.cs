e74710a0a5a732fb82927741a2b977c891e5337c
Benjamin Østner <benjamin110101@gmail.com>
2021-08-24 09:25:18 +0200

    Add files via upload

---
+++ EntityFrameworkSynopsis/EFSContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkSynopsis.PersonClasses.FemaleClasses;
+using EntityFrameworkSynopsis.PersonClasses.MaleClasses;
+
+namespace EntityFrameworkSynopsis
+{
+    class EFSContext : DbContext
+    {
+        public EFSContext() : base("EFSDB")
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+        //public DbSet<Girl> Girl { get; set; }
+        //public DbSet<Boy> Boy { get; set; }
+        //public DbSet<Woman> Woman { get; set; }
+        //public DbSet<Man> Man { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Airline> Airline { get; set; }
+        public DbSet<Review> Review { get; set; }
+        /*protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Review>().HasKey(r => new {r.Airline, r.Person});
+        }*/
+    }
+}
