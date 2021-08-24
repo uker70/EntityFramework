a7f15a03cf59640c8806d680e50879ad777e93ba
Benjamin Østner <benjamin110101@gmail.com>
2021-08-24 09:53:41 +0200

    Add files via upload

---
+++ CodeFirstEntity/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                Console.WriteLine(ctx.Database.Connection.State);
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CodeFirstEntity/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstEntity
+{
+    class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+            Database.SetInitializer<SchoolContext>(new CreateDatabaseIfNotExists<SchoolContext>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
